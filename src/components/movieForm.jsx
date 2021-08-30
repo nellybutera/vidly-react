7a15e1129757e2c845d0e07b063cb6c0ead1a136
nellybutera <85675700+nellybutera@users.noreply.github.com>
2021-08-30 11:23:45 +0200

    added images to fake movie service.js

---
+++ src/components/movieForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MovieForm = ({match}) => {
+    return ( <h1>Movie Form {match.params.id}</h1> );
+}
+ 
+export default MovieForm;
--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -8,63 +8,72 @@ const movies = [
     numberInStock: 6,
     dailyRentalRate: 2.5,
     publishDate: "2018-01-03T19:04:28.809Z",
-    liked: true
+    liked: true,
+    cover: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F5%2F5f%2FTerminator_Dark_Fate_poster.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FTerminator%3A_Dark_Fate&tbnid=cpO7Gecym0y09M&vet=12ahUKEwit47y-tNjyAhVP4oUKHexsBioQMygAegUIARCvAQ..i&docid=Gu9-FFAMg8i2PM&w=253&h=395&q=terminator%20dark%20fate&ved=2ahUKEwit47y-tNjyAhVP4oUKHexsBioQMygAegUIARCvAQ"
   },
   {
     _id: "5b21ca3eeb7f6fbccd471816",
-    title: "Die Hard",
+    title: "Vincenzo",
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 5,
-    dailyRentalRate: 2.5
+    dailyRentalRate: 2.5,
+    cover: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.kepoper.com%2Fvincenzo-kdrama-trivia%2F&psig=AOvVaw0wgNu6GvSLeyB7TsoC26z5&ust=1630401539956000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDA2pu12PICFQAAAAAdAAAAABAJ"
   },
   {
     _id: "5b21ca3eeb7f6fbccd471817",
     title: "Get Out",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 8,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    cover:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fmoviesanywhere.com%2Fmovie%2Fget-out&psig=AOvVaw1HhL_3JulPYhHCiJsAGTZl&ust=1630401607672000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKj4k7212PICFQAAAAAdAAAAABAD"
   },
   {
     _id: "5b21ca3eeb7f6fbccd471819",
     title: "Trip to Italy",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    cover: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt2967006%2F&psig=AOvVaw1EDfVcSuZMg2B8UuQKPGAy&ust=1630401654418000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOCagdC12PICFQAAAAAdAAAAABAD"
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181a",
     title: "Airplane",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    cover: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt2967006%2F&psig=AOvVaw1EDfVcSuZMg2B8UuQKPGAy&ust=1630401654418000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOCagdC12PICFQAAAAAdAAAAABAD"
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181b",
     title: "Wedding Crashers",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    cover:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fmoviesanywhere.com%2Fmovie%2Fget-out&psig=AOvVaw1HhL_3JulPYhHCiJsAGTZl&ust=1630401607672000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKj4k7212PICFQAAAAAdAAAAABAD"
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181e",
     title: "Gone Girl",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 7,
-    dailyRentalRate: 4.5
+    dailyRentalRate: 4.5,
+    cover: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F5%2F5f%2FTerminator_Dark_Fate_poster.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FTerminator%3A_Dark_Fate&tbnid=cpO7Gecym0y09M&vet=12ahUKEwit47y-tNjyAhVP4oUKHexsBioQMygAegUIARCvAQ..i&docid=Gu9-FFAMg8i2PM&w=253&h=395&q=terminator%20dark%20fate&ved=2ahUKEwit47y-tNjyAhVP4oUKHexsBioQMygAegUIARCvAQ"
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181f",
     title: "The Sixth Sense",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 4,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    cover: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.kepoper.com%2Fvincenzo-kdrama-trivia%2F&psig=AOvVaw0wgNu6GvSLeyB7TsoC26z5&ust=1630401539956000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDA2pu12PICFQAAAAAdAAAAABAJ"
   },
   {
     _id: "5b21ca3eeb7f6fbccd471821",
     title: "The Avengers",
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    cover: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt2967006%2F&psig=AOvVaw1EDfVcSuZMg2B8UuQKPGAy&ust=1630401654418000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOCagdC12PICFQAAAAAdAAAAABAD"
   }
 ];
 
